046637657a6209acdefb292ef06c7338b036c164
daniel-990 <danielarango990@gmail.com>
2023-05-15 13:28:43 -0500

    se cargan cambios de estilo

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const config = require('config');
+
+//mysql
+const conn = require('./database/db.js');
+
+//puerto
+const puerto = config.get('servidor.puerto');
+const PORT = process.env.PORT || puerto
+
+//app
+const app = express();
+//utilidades
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const urlencodedParser = bodyParser.urlencoded({extended: true});
+app.use(bodyParser.json()) // for parsing application/json
+const jsonParser = bodyParser.json();
+
+app.get('/', (req, res) => {
+    res.render('pages/index');
+})
+
+
+
+
+//corre el servidor
+app.listen(PORT, () => {
+    console.log(`se conecto al: http://localhost:${PORT}`);
+})
