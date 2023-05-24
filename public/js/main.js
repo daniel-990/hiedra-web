fbe8b29719a44da2314bad4776622d1ebd59c13c
daniel-990 <danielarango990@gmail.com>
2023-05-24 07:57:48 -0500

    cambios

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,32 @@
-const init = () => {
-    console.log("hola mundo");
+$(document).ready(function(){
 
-    const btn1 = document.getElementById("btn1");
-    const btn2 = document.getElementById("btn2");
+    $(window).scroll(function(){
+        console.log($(window).scrollTop());
+        const scroll = $(window).scrollTop();
+        const imgLogo = $("#logo");
 
+        if(scroll => 375){
+            imgLogo.attr('src','/img/c-mm.png');
+        }
+        if(scroll <= 375){
+            imgLogo.attr('src','/img/logo.png');
+        }
+    })
 
-    
-}
+    $("#gal1").hide();
+    $("#gal2").hide();
 
-init();
+    $("body").hover(function(){
+        $("#gal1").hide();
+        $("#gal2").hide();
+    });
+
+    $(".texto-intro p b a.color-negro").hover(function(){
+        $("#gal1").show();
+        $("#gal2").hide();
+    });
+    $(".texto-intro p b.btn2 a").hover(function(){
+        $("#gal2").show();
+        $("#gal1").hide();
+    });
+});
