e0a7a003511944f375097fd665606611217566cd
daniel-990 <danielarango990@gmail.com>
2023-05-25 13:44:24 -0500

    se generarn cambios en vista y portafolio

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,34 @@
 $(document).ready(function(){
 
     $(window).scroll(function(){
-        console.log($(window).scrollTop());
         const scroll = $(window).scrollTop();
         const imgLogo = $("#logo");
 
-        if(scroll => 375){
-            imgLogo.attr('src','/img/c-mm.png');
+        const sinDecimales = scroll.toFixed(2).replace(/([0-9]+(\.[0-9]+[1-9])?)(\.?0+$)/,'$1')
+        console.log(sinDecimales);
+
+        if(sinDecimales >= 100.66){
+            imgLogo.attr('src','/img/logo/c-01.svg');
         }
-        if(scroll <= 375){
-            imgLogo.attr('src','/img/logo.png');
+        if(sinDecimales <= 100.66){
+            imgLogo.attr('src','/img/logo/c-02.svg');
         }
     })
 
-    $("#gal1").hide();
-    $("#gal2").hide();
+    // $("#gal1").hide();
+    // $("#gal2").hide();
 
-    $("body").hover(function(){
-        $("#gal1").hide();
-        $("#gal2").hide();
-    });
+    // $("body").hover(function(){
+    //     $("#gal1").hide();
+    //     $("#gal2").hide();
+    // });
 
-    $(".texto-intro p b a.color-negro").hover(function(){
-        $("#gal1").show();
-        $("#gal2").hide();
-    });
-    $(".texto-intro p b.btn2 a").hover(function(){
-        $("#gal2").show();
-        $("#gal1").hide();
-    });
+    // $(".texto-intro p b a.color-negro").hover(function(){
+    //     $("#gal1").show();
+    //     $("#gal2").hide();
+    // });
+    // $(".texto-intro p b.btn2 a").hover(function(){
+    //     $("#gal2").show();
+    //     $("#gal1").hide();
+    // });
 });
